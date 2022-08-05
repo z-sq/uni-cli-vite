f566e595a0db495d422310e76084fd6c279209c1
zsan <shaoqiang_zhang@icloud.com>
2022-08-06 07:35:55 +0800

    refactor: 自定义环境变量TypeScript智能提示
    https://cn.vitejs.dev/guide/env-and-mode.html#intellisense

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,18 @@
 /// <reference types="vite/client" />
+/// <reference types="@dcloudio/types" />
 
 declare module '*.vue' {
   import { DefineComponent } from 'vue';
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>;
   export default component;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_APP_NAME: string;
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
