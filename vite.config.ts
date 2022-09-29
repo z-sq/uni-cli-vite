6bb3eddab095e3b1c7dd1c6e5c1c270c3bd0e40f
zsan <shaoqiang_zhang@icloud.com>
2022-09-29 19:09:41 +0800

    ci: 更换unocss适配uniapp依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
 import Unocss from 'unocss/vite';
-import { UnoCSSToUni } from 'unocss-preset-uni/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    uni(),
-    Unocss(),
-    // Make sure it's behind Unocss()
-    UnoCSSToUni()
-  ]
+  plugins: [uni(), Unocss()]
 });
