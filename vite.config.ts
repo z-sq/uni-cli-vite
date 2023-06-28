a82e792f28838a15e7872661e21fa5df39ecc513
zsan <shaoqiang_zhang@icloud.com>
2023-06-28 16:02:14 +0800

    feat: 安装vue调试工具

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 import Unocss from "unocss/vite";
 import checker from "vite-plugin-checker";
+import VueDevTools from "vite-plugin-vue-devtools";
 import { viteMockServe } from "vite-plugin-mock";
 
 export default defineConfig(({ command }) => {
   return {
     plugins: [
+      VueDevTools(),
       uni(),
       Unocss(),
       checker({ vueTsc: true }),
