213a1cd486fa22c99ce9eac0a99a14f181b2582b
zsan <shaoqiang_zhang@icloud.com>
2023-06-29 10:49:03 +0800

    fix: 修改图片路径引入方式
    解决在Safari浏览器图片找不到

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
   <view class="items-center justify-center" un-flex="~ col">
-    <image class="w-18 h-18 animate-bounce" un-m="x-auto t-25 b-12.5" src="/static/logo.png" />
+    <image class="w-18 h-18 animate-bounce" un-m="x-auto t-25 b-12.5" src="@/static/logo.png" />
     <view class="flex justify-center">
       <text un-text="lg gray-500">{{ title }}</text>
     </view>
